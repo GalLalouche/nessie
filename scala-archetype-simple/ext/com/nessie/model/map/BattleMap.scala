bd5742a64cc2142e9ad9c0713353632f8a958f15
gal <glpkmtg@gmail.com>
2013-05-26 23:23:54 +0300

    Added IsOccupiedTests

--- scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
@@ -1,18 +1,19 @@
 package com.nessie.model.map
 
-abstract class BattleMap(val width: Int, val height: Int) extends Traversable[(Int, Int, BattleMapObject)]{
+abstract class BattleMap(val width: Int, val height: Int) extends Traversable[(Int, Int, BattleMapObject)] {
 	require(width > 0)
 	require(height > 0)
-	
+
 	private def checkCoordinates(x: Int, y: Int) = {
 		BattleMap.check(x, width)
 		BattleMap.check(y, height)
 	}
-	
+
 	def apply(x: Int, y: Int): BattleMapObject
-	final def apply(p: MapPoint):BattleMapObject = apply(p._1, p._2)
-	def update(x: Int, y:Int, o: BattleMapObject)
+	final def apply(p: MapPoint): BattleMapObject = apply(p._1, p._2)
+	def update(x: Int, y: Int, o: BattleMapObject)
 	final def update(p: MapPoint, o: BattleMapObject): Unit = update(p._1, p._2, o)
+	final def isOccupied(x: Int, y: Int) = apply(x, y) != EmptyMapObject
 }
 
 object BattleMap {
@@ -21,4 +22,6 @@ object BattleMap {
 		case x if x >= max => throw new IndexOutOfBoundsException(x + " was larger than " + max)
 		case _ => Unit
 	}
+
+	private def tupleToPoint(x: Int, y: Int): MapPoint = (x, y)
 }
--- scala-archetype-simple/tests/com/nessie/model/map/BattleMapTests.scala
+++ scala-archetype-simple/tests/com/nessie/model/map/BattleMapTests.scala
@@ -4,27 +4,31 @@ import org.scalatest.FlatSpec
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers._
 import scala.collection.GenTraversable
+import org.scalatest.BeforeAndAfter
 
 trait CustumMatchers extends ShouldMatchers {
 	def forAll[T](right: T => Boolean) = new Matcher[GenTraversable[T]] {
 		override def apply(left: GenTraversable[T]) = {
-					val leftPretty = left.take(3) + {if (left.size > 3) "..." else ""}
-					MatchResult(left.forall(right),
-					right + " does not apply to all of " + leftPretty,
-					right + " applies too all of " + leftPretty)
+			val leftPretty = left.take(3) + { if (left.size > 3) "..." else "" }
+			MatchResult(left.forall(right),
+				right + " does not apply to all of " + leftPretty,
+				right + " applies too all of " + leftPretty)
 		}
 	}
 }
 
-class BattleMapTests extends FlatSpec with ShouldMatchers with MockFactory with CustumMatchers {
+class BattleMapTests extends FlatSpec with ShouldMatchers with MockFactory with CustumMatchers with BeforeAndAfter {
 	var $: BattleMap = new ArrayBattleMap(5, 10)
-	"Map Contructor" should "return the correct height and width" in {
+	private def mockObject = mock[BattleMapObject]
+	val o = mockObject
+
+	before($ = new ArrayBattleMap(5, 10))
+
+	"Contructor" should "return the correct height and width" in {
 		$ = new ArrayBattleMap(5, 10);
 		$.width should be === 5
 		$.height should be === 10
 	}
-
-	private def mockObject = mock[BattleMapObject]
 
 	it should "throw IllegalArgumentException on negative height" in {
 		evaluating { new ArrayBattleMap(-3, 10) } should produce[IllegalArgumentException]
@@ -34,7 +38,7 @@ class BattleMapTests extends FlatSpec with ShouldMatchers with MockFactory with 
 		$.size should be === 50
 	}
 
-	"Map Apply" should "throw IllegalArgumentException on zero width" in {
+	"Apply" should "throw IllegalArgumentException on zero width" in {
 		evaluating { new ArrayBattleMap(5, 0) } should produce[IllegalArgumentException]
 	}
 
@@ -49,26 +53,31 @@ class BattleMapTests extends FlatSpec with ShouldMatchers with MockFactory with 
 	it should "start out as all empty" in {
 		$ should forAll[(Int, Int, BattleMapObject)](x => x._3 == EmptyMapObject);
 	}
-	
+
 	it should "also work with an (Int, Int) parameter)" in {
-		$((0,0)) should be === EmptyMapObject
+		$((0, 0)) should be === EmptyMapObject
 	}
 
-	"Map update" should "place an object" in {
-		val m = mockObject
-		$(0, 0) = m
-		$(0, 0) should be === m
+	"Update" should "place an object" in {
+		$(0, 0) = o
+		$(0, 0) should be === o
 	}
-	
-	it should "untuple the point parameter correctly" in { 
-		val m = mockObject;
-		$(3, 5) = m;
-		$((3, 5)) should be === m
+
+	it should "untuple the point parameter correctly" in {
+		$(3, 5) = o;
+		$((3, 5)) should be === o
 		val m2 = mockObject
 		$((4, 2)) = m2
 		$(4, 2) should be === m2
 	}
-		
-}
 
+	"IsOccupied" should "return false on empty slot" in {
+		$.isOccupied(0, 0) should be === false
+	}
+
+	it should "return true on occupied slot" in {
+		$(0, 0) = o;
+		$.isOccupied(0, 0) should be === true
+	}
+}
 
