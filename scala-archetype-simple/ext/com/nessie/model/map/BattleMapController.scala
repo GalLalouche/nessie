26388e87afd44b13d62bcac9e5543d2fe8ae4266
Gal Lalouche <Gal Lalouche@gal-PC.mshome.net>
2013-05-27 15:53:07 +0300

    Some refactoring

--- scala-archetype-simple/ext/com/nessie/model/map/BattleMapController.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/BattleMapController.scala
@@ -5,8 +5,7 @@ class BattleMapController(map: BattleMap) {
 	require(map != null)
 	
 	final def apply(p: MapPoint) = if (isOccupied(p)) map(p) else throw new MapEmptyException(p)
-	final def update(p: MapPoint, o: BattleMapObject): Unit = if (isOccupied(p) == false) map(p) = o else throw new MapOccupiedException(p)
-	final def isOccupied(x: Int, y: Int): Boolean = isOccupied(BattleMap.tupleToPoint(x, y))
+	final def update(p: MapPoint, o: BattleMapObject) = if (isOccupied(p)) throw new MapOccupiedException(p) else map(p) = o
 	final def isOccupied(p: MapPoint): Boolean = map(p) != EmptyMapObject
 	final def remove(p: MapPoint): BattleMapObject = {
 		val $ = this(p)
