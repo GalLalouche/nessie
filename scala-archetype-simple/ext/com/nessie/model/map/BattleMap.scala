75a262cd86bace4985e83616148dd62a3da9d5ca
lalouche <lalouche@lap304.cs.technion.ac.il>
2013-05-27 23:49:34 +0300

    Some slight refactoring

--- scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
@@ -4,21 +4,6 @@ abstract class BattleMap(val width: Int, val height: Int) extends Traversable[(I
 	require(width > 0)
 	require(height > 0)
 
-	private def checkCoordinates(x: Int, y: Int) = {
-		BattleMap.check(x, width)
-		BattleMap.check(y, height)
-	}
-
 	def apply(p: MapPoint): BattleMapObject
 	def update(p: MapPoint, o: BattleMapObject): Unit
-}
-
-object BattleMap {
-	private def check(input: Int, max: Int) = input match {
-		case x if x < 0 => throw new IndexOutOfBoundsException(x + " was negative")
-		case x if x >= max => throw new IndexOutOfBoundsException(x + " was larger than " + max)
-		case _ => Unit
-	}
-
-	def tupleToPoint(x: Int, y: Int): MapPoint = (x, y)
 }
--- scala-archetype-simple/ext/com/nessie/model/map/BattleMapController.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/BattleMapController.scala
@@ -4,15 +4,15 @@ package com.nessie.model.map
 class BattleMapController(map: BattleMap) {
 	require(map != null)
 	
-	final def apply(p: MapPoint) = if (isOccupied(p)) map(p) else throw new MapEmptyException(p)
-	final def update(p: MapPoint, o: BattleMapObject) = if (isOccupied(p)) throw new MapOccupiedException(p) else map(p) = o
-	final def isOccupied(p: MapPoint): Boolean = map(p) != EmptyMapObject
-	final def remove(p: MapPoint): BattleMapObject = {
+	def apply(p: MapPoint) = if (isOccupied(p)) map(p) else throw new MapEmptyException(p)
+	def update(p: MapPoint, o: BattleMapObject) = if (isOccupied(p)) throw new MapOccupiedException(p) else map(p) = o
+	def isOccupied(p: MapPoint): Boolean = map(p) != EmptyMapObject
+	def remove(p: MapPoint): BattleMapObject = {
 		val $ = this(p)
 		map(p) = EmptyMapObject
 		$
 	}
-	final def move(src: MapPoint) = {
+	def move(src: MapPoint) = {
 		val o = this(src)
 		new {
 			def to(dst: MapPoint) = {
