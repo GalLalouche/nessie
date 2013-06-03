b5c273442520ba4422749cd5842e090e4c40a6e3
Gal Lalouche <Gal Lalouche@gal-PC.mshome.net>
2013-06-03 12:12:46 +0300

    Moved to new packer

--- scala-archetype-simple/ext/com/nessie/view/map/StringBattleMapViewer.scala
+++ scala-archetype-simple/ext/com/nessie/view/map/StringBattleMapViewer.scala
@@ -1,4 +1,8 @@
-package com.nessie.model.map
+package com.nessie.view.map
+
+import com.nessie.model.map.BattleMap
+import com.nessie.model.map.BattleMapObject
+import com.nessie.model.map.EmptyMapObject
 
 class StringBattleMapViewer(m: BattleMap) {
 	override def toString() = {
---
+++ scala-archetype-simple/tests/com/nessie/view/map/StringBattleMapViewerTests.scala
@@ -0,0 +1,20 @@
+package com.nessie.view.map
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfter
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+import com.nessie.model.map.ArrayBattleMap
+
+class StringBattleMapViewerTests extends FlatSpec with ShouldMatchers with MockFactory with BeforeAndAfter {
+	"StringViewer" should "print all _ for empty map" in {
+		val map = new ArrayBattleMap(2, 3);
+		val $ = new StringBattleMapViewer(map)
+		$.toString should be ===
+			"_,_\n" +
+			"_,_\n" +
+			"_,_\n";
+	}
+}
+
