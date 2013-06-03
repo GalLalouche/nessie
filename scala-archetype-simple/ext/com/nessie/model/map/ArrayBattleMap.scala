2c623aecc4d660b68817ac71188bcfe46eae05ca
lalouche <lalouche@lap304.cs.technion.ac.il>
2013-06-03 09:11:26 +0300

    Moved foreach to father

--- scala-archetype-simple/ext/com/nessie/model/map/ArrayBattleMap.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/ArrayBattleMap.scala
@@ -3,14 +3,7 @@ package com.nessie.model.map
 class ArrayBattleMap(width: Int, height: Int) extends BattleMap(width, height) {
 	val array = Array.ofDim[BattleMapObject](width, height)
 	for (cell <- this.map(x => (x._1, x._2))) this(cell) = EmptyMapObject
-	override def foreach[T](f: ((Int, Int, BattleMapObject)) => T) = {
-		for (
-			x <- 0 until array.length;
-			y <- 0 until array(0).length
-		) {
-			f(x, y, array(x)(y))
-		}
-	}
+	
 
 	override def apply(p: MapPoint) = array(p._1)(p._2)
 	override def update(p: MapPoint, o: BattleMapObject) = array(p._1)(p._2) = o
--- scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/BattleMap.scala
@@ -3,6 +3,15 @@ package com.nessie.model.map
 abstract class BattleMap(val width: Int, val height: Int) extends Traversable[(Int, Int, BattleMapObject)] {
 	require(width > 0)
 	require(height > 0)
+	
+	override def foreach[T](f: ((Int, Int, BattleMapObject)) => T) = {
+		for (
+			x <- 0 until width;
+			y <- 0 until height
+		) {
+			f(x, y, this(x, y))
+		}
+	}
 
 	def apply(p: MapPoint): BattleMapObject
 	def update(p: MapPoint, o: BattleMapObject): Unit
