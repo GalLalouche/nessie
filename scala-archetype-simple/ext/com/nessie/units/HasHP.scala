b48ccba75c58da352d4dd7be3c07d100c8bf8763
gal <glpkmtg@gmail.com>
2013-06-05 01:05:52 +0300

    test

--- scala-archetype-simple/ext/com/nessie/units/HasHP.scala
+++ scala-archetype-simple/ext/com/nessie/units/HasHP.scala
@@ -9,19 +9,19 @@ package com.nessie.units
  */
 class HasHP private(val currentHp: Int, val maxHp: Int) {
 	def healHp(i: Int) = {
-		require(i >= 0)
-		new HasHP(Math.min(currentHp + i, maxHp), maxHp)
+		require (i >= 0)
+		new HasHP (Math.min (currentHp + i, maxHp), maxHp)
 	}
 
-	require(currentHp >= 0)
-	require(currentHp <= maxHp)
-	require(maxHp > 0)
+	require (currentHp >= 0)
+	require (currentHp <= maxHp)
+	require (maxHp > 0)
 
-	def this(maxHp: Int) = this(maxHp, maxHp)
+
+	def this(maxHp: Int) = this (maxHp, maxHp)
 
 	def reduceHp(i: Int) = {
-		require(i >= 0)
-		new HasHP(Math.max(maxHp - i, 0), maxHp)
+		require (i >= 0)
+		new HasHP (Math.max (maxHp - i, 0), maxHp)
 	}
-
 }
