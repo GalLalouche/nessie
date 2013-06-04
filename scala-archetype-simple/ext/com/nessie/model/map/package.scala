60953107a34e94a1cada6ce16049ae2b08c9f57d
gal <glpkmtg@gmail.com>
2013-06-04 14:05:06 +0300

    Implemented hashcode and equals

--- scala-archetype-simple/ext/com/nessie/model/map/package.scala
+++ scala-archetype-simple/ext/com/nessie/model/map/package.scala
@@ -1,7 +1,17 @@
 package com.nessie.model
 
 package object map {
+
 	class MapPoint(val x: Int, val y: Int) {
 		override def toString() = (x, y).toString
+
+		override def equals(p: Any) = {
+			if (p.isInstanceOf[MapPoint] == false) false
+			val other = p.asInstanceOf[MapPoint];
+			other.x == x && other.y == y
+		}
+
+		override def hashCode(): Int = x.hashCode * 17 + y.hashCode
 	}
+
 }
--- scala-archetype-simple/ext/com/nessie/view/map/swing/MapPanel.scala
+++ scala-archetype-simple/ext/com/nessie/view/map/swing/MapPanel.scala
@@ -3,20 +3,22 @@ package com.nessie.view.map.swing
 import scala.swing.GridPanel
 import com.nessie.model.map.BattleMap
 import scala.swing.Button
-import scala.swing.event.ButtonClicked
 import com.nessie.model.map.objects.BattleMapObject
 import com.nessie.units.Warrior
 import com.nessie.model.map.objects.EmptyMapObject
 import com.nessie.units.Skeleton
-import scala.swing.Publisher
 import scala.swing.Action
 import com.nessie.model.map.MapPoint
 import scala.swing.event.Event
-import scala.swing.event.ActionEvent
 
-case class CellClicked(p:MapPoint) extends Event
+case class CellClicked(p: MapPoint) extends Event {
+	override def equals(obj: Any) = obj.isInstanceOf[CellClicked] && obj.asInstanceOf[CellClicked].p == p
+
+	override def hashCode(): Int = p.hashCode
+}
+
 class MapPanel(map: BattleMap) extends GridPanel(map.width, map.height) {
-	require (map != null)
+	require(map != null)
 	contents ++= map map (x => {
 		val b = new Button(Action(print(x._2)) {
 			publish(CellClicked(x._1))
@@ -30,4 +32,6 @@ class MapPanel(map: BattleMap) extends GridPanel(map.width, map.height) {
 		case _: Warrior => "w"
 		case _: Skeleton => "s"
 	}
+
+	override def toString = "MapPanel"
 }
