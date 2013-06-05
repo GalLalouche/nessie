dd58b35b677f914eaa9b3576096241f7b12eaf31
gal <glpkmtg@gmail.com>
2013-06-05 13:37:56 +0300

    Added combat unit + tests

--- scala-archetype-simple/ext/com/nessie/units/HasHP.scala
+++ scala-archetype-simple/ext/com/nessie/units/HasHP.scala
@@ -1,27 +1,19 @@
 package com.nessie.units
 
-/**
- * Created with IntelliJ IDEA.
- * User: gal
- * Date: 04/06/13
- * Time: 23:04
- * To change this template use File | Settings | File Templates.
- */
-class HasHP private(val currentHp: Int, val maxHp: Int) {
-	def healHp(i: Int) = {
-		require (i >= 0)
-		new HasHP (Math.min (currentHp + i, maxHp), maxHp)
-	}
-
+class HasHP protected(val currentHp: Int, val maxHp: Int) {
 	require (currentHp >= 0)
 	require (currentHp <= maxHp)
 	require (maxHp > 0)
-
 
 	def this(maxHp: Int) = this (maxHp, maxHp)
 
 	def reduceHp(i: Int) = {
 		require (i >= 0)
 		new HasHP (Math.max (maxHp - i, 0), maxHp)
+	}
+
+	def healHp(i: Int) = {
+		require (i >= 0)
+		new HasHP (Math.min (currentHp + i, maxHp), maxHp)
 	}
 }
--- scala-archetype-simple/tests/com/nessie/units/HasHPTests.scala
+++ scala-archetype-simple/tests/com/nessie/units/HasHPTests.scala
@@ -1,65 +1,60 @@
 package com.nessie.units
 
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.BeforeAndAfter
-import org.scalatest.FlatSpec
+import org.scalatest.{OneInstancePerTest, FlatSpec}
 import org.scalatest.matchers._
 
-class HasHPTests extends FlatSpec with ShouldMatchers with MockFactory with BeforeAndAfter {
-	var $: HasHP = null
-
-	before({
-		$ = new HasHP(10)
-	})
+class HasHPTests extends FlatSpec with ShouldMatchers with MockFactory with OneInstancePerTest {
+	def $: HasHP = new HasHP (10)
 
 	"Constructor" should "return the correct maxHP" in {
-		new HasHP(10).maxHp should be === 10
+		new HasHP (10).maxHp should be === 10
 	}
 
 	it should "throw IllegalArgumentException on zero hp" in {
 		evaluating {
-			new HasHP(0)
+			new HasHP (0)
 		} should produce[IllegalArgumentException]
 	}
 
 	it should "have the same max hp and current hp" in {
-		new HasHP(10).currentHp should be === 10
+		new HasHP (10).currentHp should be === 10
 	}
 
 	"Reduce" should "throw exception on negative amount" in {
 		evaluating {
-			$.reduceHp(-1)
+			$.reduceHp (-1)
 		} should produce[IllegalArgumentException]
 	}
 
 	it should "return an object with the new current HP" in {
-		$.reduceHp(5).currentHp should be === 5
+		$.reduceHp (5).currentHp should be === 5
 	}
 
 	it should "return an object with the same max HP" in {
-		$.reduceHp(5).maxHp should be === 10
+		$.reduceHp (5).maxHp should be === 10
 	}
 
 	it should "return an object with 0 hp if reduction is larger than current" in {
-		$.reduceHp(11).currentHp should be === 0
+		$.reduceHp (11).currentHp should be === 0
 	}
 
 	"Heal" should "throw exception on negative amount" in {
 		evaluating {
-			$.healHp(-1)
+			$.healHp (-1)
 		} should produce[IllegalArgumentException]
 	}
 
 	it should "return an object with the new current HP" in {
-		$.reduceHp(5).healHp(3).currentHp should be === 8
+		$.reduceHp (5).healHp (3).currentHp should be === 8
 	}
 
 	it should "return an object with the same max HP" in {
-		$.reduceHp(5).healHp(3).maxHp should be === 10
+		$.reduceHp (5).healHp (3).maxHp should be === 10
 	}
 
 	it should "return an object with max hp if heal + current is larger than max" in {
-		$.reduceHp(5).healHp(10).currentHp should be === 10
+		$.reduceHp (5).healHp (10).currentHp should be === 10
 	}
 }
 
