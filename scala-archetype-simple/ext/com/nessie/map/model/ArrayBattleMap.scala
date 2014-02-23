4fd7c3f24e3fb5ba7c3f8c96e6e89b1d14228756
gal_lalouche <glpkmtg@gmail.com>
2014-02-23 17:25:20 +0200

    changed list to vector

--- scala-archetype-simple/ext/com/nessie/map/model/ArrayBattleMap.scala
+++ scala-archetype-simple/ext/com/nessie/map/model/ArrayBattleMap.scala
@@ -7,7 +7,7 @@ import com.nessie.model.map.objects.EmptyMapObject
  * Implements a map as a List[List] matrix
  * @param list The matrix list
  */
-class ArrayBattleMap private(list: List[List[BattleMapObject]]) extends BattleMap(list.length, list(0).length) {
+class ArrayBattleMap private(list: Vector[Vector[BattleMapObject]]) extends BattleMap(list.length, list(0).length) {
 	override def apply(p: MapPoint) = list(p.x)(p.y)
 
 	override def place(p: MapPoint, o: BattleMapObject) = new ArrayBattleMap(list.updated(p.x, list(p.x).updated(p.y, o)))
@@ -23,6 +23,6 @@ object ArrayBattleMap {
 		// these are required as otherwise the list.fill will throw an IndexOutOfBoundsException, which is not what we want
 		require(width > 0)
 		require(height > 0)
-		List.fill(width)(List.fill[BattleMapObject](height)(EmptyMapObject))
+		Vector.fill(width)(Vector.fill(height)(EmptyMapObject))
 	})
 }
