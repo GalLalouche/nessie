e0a31c9966923e1bbae42d7d7e142daaf4293c5d
gal_lalouche <glpkmtg@gmail.com>
2014-02-23 17:24:10 +0200

    added some javadocs

--- scala-archetype-simple/ext/com/nessie/map/exceptions/MapEmptyException.scala
+++ scala-archetype-simple/ext/com/nessie/map/exceptions/MapEmptyException.scala
@@ -2,4 +2,7 @@ package com.nessie.map.exceptions
 
 import com.nessie.map.model.MapPoint
 
-class MapEmptyException(p: MapPoint) extends RuntimeException("Map is empty at " + p)
+/**
+ * Thrown when someone is trying to access a point that is empty 
+ */
+class MapEmptyException(val p: MapPoint) extends RuntimeException("Map is empty at " + p)
--- scala-archetype-simple/ext/com/nessie/map/exceptions/MapOccupiedException.scala
+++ scala-archetype-simple/ext/com/nessie/map/exceptions/MapOccupiedException.scala
@@ -2,4 +2,7 @@ package com.nessie.map.exceptions
 
 import com.nessie.map.model.MapPoint
 
-class MapOccupiedException(p: MapPoint) extends RuntimeException("Map is occupied at " + p)
+/**
+ * Thrown when someone is trying to place a unit in a non-empty square
+ */
+class MapOccupiedException(val p: MapPoint) extends RuntimeException("Map is occupied at " + p)
