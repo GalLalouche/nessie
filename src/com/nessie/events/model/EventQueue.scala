bfcb136814022424597127373d67a6e397bce75f
U-Gal-PC\Gal <Gal@Gal-PC.(none)>
2014-08-31 23:09:29 +0300

    added a bunch of logic to EQ

--- src/com/nessie/events/model/EventQueue.scala
+++ src/com/nessie/events/model/EventQueue.scala
@@ -2,21 +2,36 @@ package com.nessie.events.model
 
 import common.rich.collections.RichSeq._
 
-class EventQueue private(q: List[(Event, Double)]) extends Iterable[Event] {
-  private implicit def qToEQ(eq: Seq[(Event, Double)]) = new EventQueue(eq.toList)
+class EventQueue[T] private(q: List[(T, Double)], currentDelay: Double) extends Iterable[T] {
+  private implicit def qToEQ(eq: (List[(T, Double)], Double)) = new EventQueue[T](eq._1, eq._2)
 
   // this ensures by induction that there are no null events
-  require(q.isEmpty || q.head != null)
+  require(q.isEmpty || q.head._1 != null)
 
-  def this() = this(List[(Event, Double)]())
+  def this() = this(List[(T, Double)](), 0.0)
   override def iterator = q.iterator.map(_._1)
 
-  def +(e: Event): EventQueue = add(e, 0.0)
-  def add(e: Event, withDelay: Double): EventQueue = add((e, withDelay))
-  private def add(eventWithDelay: (Event, Double)): EventQueue = q
-    .findIndex(_._2 > eventWithDelay._2)
-    .map(i => q insert eventWithDelay at i)
-    .getOrElse((eventWithDelay :: q.reverse).reverse)
-    .toList
-  def next: EventQueue = new EventQueue(q.tail)
+  def +(e: T): EventQueue[T] = add(e, 0.0)
+  def add(e: T, withDelay: Double): EventQueue[T] = add((e, withDelay + currentDelay))
+  private def add(eventWithDelay: (T, Double)): EventQueue[T] =
+    (q
+      .findIndex(_._2 > eventWithDelay._2)
+      .map(i => q insert eventWithDelay at i)
+      .getOrElse((eventWithDelay :: q.reverse).reverse)
+      .toList, currentDelay)
+  def next: EventQueue[T] = (q.tail, q.head._2 + currentDelay)
+  def repeat(e: T) = new {
+    def times(n: Int) = new {
+      require(n > 0, "The number of repeats must be a positive integer")
+      def inIntervalsOf(delay: Double) = new {
+        require(delay >= 0, "The intervals length must non-negative")
+        def withDefaultDelay = withDelay(delay)
+        def withoutDelay = withDelay(0)
+        def withDelay(startingDelay: Double): EventQueue[T] = {
+          require(startingDelay >= 0, "Starting delay must be non-negative")
+          (1 until n).foldLeft(add(e, startingDelay))((eq, i) => eq.add(e, startingDelay + delay * i))
+        }
+      }
+    }
+  }
 }
--- tests/com/nessie/events/model/EventQueueTests.scala
+++ tests/com/nessie/events/model/EventQueueTests.scala
@@ -3,30 +3,48 @@ package com.nessie.events.model
 import common.AuxSpecs
 import org.scalatest.FreeSpec
 import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
 
 class EventQueueTests extends FreeSpec with AuxSpecs with MockitoSugar {
-  val eq = new EventQueue
+  val eq = new EventQueue[Event]
   val e = mock[Event]
-  "+" - {
-    val $ = eq + e
+  when(e.toString).thenReturn("e")
+  val e2 = mock[Event]
+  when(e2.toString).thenReturn("e2")
+  val e3 = mock[Event]
+  when(e3.toString).thenReturn("e3")
+  "adding" - {
+    "throws exception null" in {
+      an[IllegalArgumentException] should be thrownBy {eq + null}
+    }
     "no delay" - {
-      "should return a queue with the added element" in {$.head shouldReturn e}
-      "have size 1" in {$.size shouldReturn 1}
-      "next should return an empty EQ" in {$.next.isEmpty shouldReturn true}
+      "should return a queue with only the added element" in {(eq + e).toSeq shouldReturn List(e)}
     }
     "with delay" - {
-      val e2 = mock[Event]
-      "first with delay, then without" - {
-        val $ = eq.add(e, 0.5) + e2
-        "should retain its head if delay is longer than current" in {$.head shouldReturn e2}
-        "should have size 2" in {$.size shouldReturn 2}
-        "next should return the delayed object" in {$.next.toSeq shouldReturn List(e)}
+      "first with delay, then without should return e2 before e" in {(eq.add(e, 0.5) + e2).toSeq shouldReturn List(e2, e)}
+      "first without delay, then with should return e then e2" in {(eq + e add(e2, 0.5)).toSeq shouldReturn List(e, e2)}
+    }
+    "in between" in {(eq.add(e, 0).add(e3, 0.5).add(e2, 0.3)).toSeq shouldReturn List(e, e2, e3)}
+    "after next" in {eq.add(e, 0.5).add(e3, 1.5).next.add(e2, 1.0).toSeq shouldReturn List(e3, e2)}
+  }
+  "adding with repeats" - {
+    "finite" - {
+      "no other data" in {eq.repeat(e).times(3).inIntervalsOf(0).withDefaultDelay.toSeq shouldReturn List(e, e, e)}
+      "interleaved with other data" in {
+        eq.add(e, 0).add(e2, 1).repeat(e3).times(2).inIntervalsOf(0.6).withDefaultDelay.toList shouldReturn List(e, e3, e2, e3)
       }
-      "first without delay, then with" - {
-        val $ = eq + e add(e2, 0.5)
-        "should retain its head if delay is longer than current" in {$.head shouldReturn e}
-        "should have size 2" in {$.size shouldReturn 2}
-        "next should return the delayed object" in {$.next.toSeq shouldReturn List(e2)}
+      "interleaved with other data without delay" in {
+        eq.add(e, 0.3).add(e2, 0.6).repeat(e3).times(3).inIntervalsOf(0.4).withoutDelay.toList shouldReturn List(e3, e, e3, e2, e3)
+      }
+      "interleaved with other data with non-default delay" in {
+        eq.add(e, 0.5).add(e2, 1.0).repeat(e3).times(3).inIntervalsOf(0.3).withDelay(0.6).toList shouldReturn List(e, e3, e3, e2, e3)
+      }
+      "should work on edge cases" in {eq.repeat(e).times(1).inIntervalsOf(0).withoutDelay.toSeq shouldReturn List(e)}
+      "throw an exception on illegal inputs" in {
+        an[IllegalArgumentException] should be thrownBy {eq.repeat(e).times(0)}
+        an[IllegalArgumentException] should be thrownBy {eq.repeat(e).times(-1)}
+        an[IllegalArgumentException] should be thrownBy {eq.repeat(e).times(1).inIntervalsOf(-1)}
+        an[IllegalArgumentException] should be thrownBy {eq.repeat(e).times(1).inIntervalsOf(0).withDelay(-1)}
       }
     }
   }
