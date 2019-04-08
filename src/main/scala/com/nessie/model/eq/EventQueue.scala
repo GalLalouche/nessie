361761b7ff807a1ce4e18d37f7fa69db0c15764f
Gal PC <glpkmtg@gmmail.com>
2019-04-08 20:46:40 +0300

    Cleaning EventQueue a bit

--- src/main/scala/com/nessie/model/eq/EventQueue.scala
+++ src/main/scala/com/nessie/model/eq/EventQueue.scala
@@ -4,35 +4,31 @@ import common.rich.RichT._
 import common.rich.collections.RichSeq._
 import monocle.syntax.{ApplySyntax, FieldsSyntax}
 
-class EventQueue[T <: AnyRef] private(
-    private val q: List[(T, Double)],
+class EventQueue[A] private(
+    private val q: List[(A, Double)],
     currentDelay: Double,
-    infinites: Map[T, Double],
-) extends Iterable[T]
+    infinites: Map[A, Double],
+) extends Iterable[A]
     with ApplySyntax with FieldsSyntax {
-  private def qToEQ(eq: (List[(T, Double)], Double, Map[T, Double])) = new EventQueue[T](eq._1, eq._2, eq._3)
 
   // this ensures by induction that there are no null events
   require(q.isEmpty || q.head._1 != null)
 
-  def this() = this(List[(T, Double)](), 0.0, Map())
-  def partialMap(f: PartialFunction[T, T]): EventQueue[T] = {
-    val orNop: T => T = f.orElse {
-      case e => e
-    }
-    def mapTuple(e: (T, Double)): (T, Double) = e.copy(_1 = orNop(e._1))
+  def this() = this(List[(A, Double)](), 0.0, Map())
+  /** Unlike collect, partialMap does not modify elements the partial function does not apply to. */
+  def partialMap(f: PartialFunction[A, A]): EventQueue[A] = {
+    val orNop: A => A = f.orElse {case e => e}
+    def mapTuple(e: (A, Double)): (A, Double) = e.copy(_1 = orNop(e._1))
     new EventQueue(q.map(mapTuple), currentDelay, infinites.map(mapTuple))
   }
-  def remove(f: PartialFunction[T, Boolean]): EventQueue[T] = {
-    val orKeep: T => Boolean = f.orElse {
-      case _ => false
-    }
+  def remove(f: PartialFunction[A, Boolean]): EventQueue[A] = {
+    val orKeep: A => Boolean = f.orElse(false.partialConst)
     new EventQueue(q.filterNot(e => orKeep(e._1)), currentDelay, infinites.filterNot(e => orKeep(e._1)))
   }
-  override def iterator = new Iterator[T] {
+  override def iterator = new Iterator[A] {
     private var current = EventQueue.this
     override def hasNext: Boolean = current.q.nonEmpty
-    override def next(): T = {
+    override def next(): A = {
       val $ = current.q.head._1
       current = current.tail
       $
@@ -41,19 +37,24 @@ class EventQueue[T <: AnyRef] private(
 
   override def toString: String = this.take(3).mkString(", ") + "..."
 
-  def +(e: T): EventQueue[T] = add(e, 0.0)
-  def add(e: T, withDelay: Double): EventQueue[T] = add((e, withDelay + currentDelay))
-  private def add(eventWithDelay: (T, Double), infinites: Map[T, Double] = this.infinites): EventQueue[T] =
-    qToEQ((q.findIndex(_._2 > eventWithDelay._2).map(i => q insert eventWithDelay at i).getOrElse((eventWithDelay :: q.reverse).reverse).toList,
-        currentDelay,
-        infinites))
-  override def tail: EventQueue[T] = {
+  def +(e: A): EventQueue[A] = add(e, 0.0)
+  def add(e: A, withDelay: Double): EventQueue[A] = add((e, withDelay + currentDelay))
+  private def add(eventWithDelay: (A, Double), infinites: Map[A, Double] = this.infinites): EventQueue[A] =
+    new EventQueue(
+      q.findIndex(_._2 > eventWithDelay._2)
+          .map(i => q insert eventWithDelay at i)
+          .getOrElse((eventWithDelay :: q.reverse).reverse)
+          .toList,
+      currentDelay,
+      infinites,
+    )
+  override def tail: EventQueue[A] = {
     val pop = q.head._1
-    qToEQ((q.tail, q.head._2, infinites))
+    new EventQueue(q.tail, q.head._2, infinites)
         .mapIf(_ => infinites contains pop) // re-append head if is infinite
         .to(_.repeat(pop).infinitely.inIntervalsOf(infinites(pop)))
   }
-  def repeat(e: T) = new {
+  def repeat(e: A) = new {
     def times(n: Int) = new {
       require(n > 0, "The number of repeats must be a positive integer")
       // TODO extract in Intervals of trait
@@ -61,17 +62,16 @@ class EventQueue[T <: AnyRef] private(
         require(delay >= 0, "The intervals length must non-negative")
         def withDefaultDelay = withDelay(delay)
         def withoutDelay = withDelay(0)
-        def withDelay(startingDelay: Double): EventQueue[T] = {
+        def withDelay(startingDelay: Double): EventQueue[A] = {
           require(startingDelay >= 0, "Starting delay must be non-negative")
-          (1 until n).foldLeft(add(e, startingDelay))((eq, i) => eq.add(e, startingDelay + delay * i))
+          1.until(n).foldLeft(add(e, startingDelay))((eq, i) => eq.add(e, startingDelay + delay * i))
         }
       }
     }
     def infinitely = new {
-      def inIntervalsOf(d: Double): EventQueue[T] = {
+      def inIntervalsOf(d: Double): EventQueue[A] = {
         require(d > 0, "Infinite intervals must be positive")
-        val newMap = infinites + (e -> d)
-        add((e, d + currentDelay), newMap)
+        add((e, d + currentDelay), infinites = infinites + (e -> d))
       }
     }
   }
