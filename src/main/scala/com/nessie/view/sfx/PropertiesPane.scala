959cba10d2ae9b49fdd1ae732118acae3a3d344d
Gal PC <glpkmtg@gmmail.com>
2019-04-08 22:52:35 +0300

    Remove JavaConversions

--- src/main/scala/com/nessie/view/sfx/PropertiesPane.scala
+++ src/main/scala/com/nessie/view/sfx/PropertiesPane.scala
@@ -6,12 +6,12 @@ import common.rich.RichT._
 import javafx.scene.control.Label
 import scalafx.scene.layout.VBox
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 private class PropertiesPane(gs: GameState) {
   val highlighter = new Highlighter[CombatUnit] {
     override def highlight(u: CombatUnit) =
-      node.children.setAll(List(u.simpleName, s"${u.currentHp}/${u.maxHp}").map(new Label(_)))
+      node.children.setAll(List(u.simpleName, s"${u.currentHp}/${u.maxHp}").map(new Label(_)).asJava)
     override def disableHighlighting(u: CombatUnit) = node.children.clear()
   }
 
