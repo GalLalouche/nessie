9e415866a1b8591378c14546ee436ad308918688
Gal Lalouche <gal.lalouche@oath.com>
2019-04-16 16:26:51 +0300

    Cleanup BattleMapTest

--- src/test/scala/com/nessie/model/map/BattleMapTest.scala
+++ src/test/scala/com/nessie/model/map/BattleMapTest.scala
@@ -2,18 +2,17 @@ package com.nessie.model.map
 
 import common.AuxSpecs
 import org.scalatest.FreeSpec
-
 import scalax.collection.GraphEdge.UnDiEdge
 
 abstract class BattleMapTest extends FreeSpec with AuxSpecs {
   protected def createBattleMap(width: Int, height: Int): BattleMap
 
   "Constructor" - {
-    "Negative width" in { an[IllegalArgumentException] should be thrownBy createBattleMap(-3, 10) }
-    "Zero width" in { an[IllegalArgumentException] should be thrownBy createBattleMap(0, 10) }
-    "Negative height" in { an[IllegalArgumentException] should be thrownBy createBattleMap(3, -10) }
-    "Zero height" in { an[IllegalArgumentException] should be thrownBy createBattleMap(3, 0) }
-    "starts out as all empty" in { DictBattleMap(10, 20).points.map(_._2).forall(_ == EmptyMapObject) shouldReturn true }
+    "Negative width" in {an[IllegalArgumentException] should be thrownBy createBattleMap(-3, 10)}
+    "Zero width" in {an[IllegalArgumentException] should be thrownBy createBattleMap(0, 10)}
+    "Negative height" in {an[IllegalArgumentException] should be thrownBy createBattleMap(3, -10)}
+    "Zero height" in {an[IllegalArgumentException] should be thrownBy createBattleMap(3, 0)}
+    "starts out as all empty" in {DictBattleMap(10, 20).points.map(_._2).forall(_ == EmptyMapObject) shouldReturn true}
   }
   "Attributes" in {
     val $ = createBattleMap(width = 5, height = 10)
@@ -40,16 +39,16 @@ abstract class BattleMapTest extends FreeSpec with AuxSpecs {
       }
     }
     "Place on non-empty" - {
-      "Object" in { an[MapOccupiedException] should be thrownBy $.place(objectPoint, NonEmptyBattleMapObject) }
-      "Between" in { an[MapOccupiedException] should be thrownBy $.place(betweenPoints, NonEmptyBetweenMapObject) }
+      "Object" in {an[MapOccupiedException] should be thrownBy $.place(objectPoint, NonEmptyBattleMapObject)}
+      "Between" in {an[MapOccupiedException] should be thrownBy $.place(betweenPoints, NonEmptyBetweenMapObject)}
     }
     "Place empty" - {
-      "Object" in { an[IllegalArgumentException] should be thrownBy $.place(emptyPoint, EmptyMapObject) }
-      "Between" in { an[IllegalArgumentException] should be thrownBy $.place(emptyBetween, EmptyBetweenMapObject) }
+      "Object" in {an[IllegalArgumentException] should be thrownBy $.place(emptyPoint, EmptyMapObject)}
+      "Between" in {an[IllegalArgumentException] should be thrownBy $.place(emptyBetween, EmptyBetweenMapObject)}
     }
     "Remove empty" - {
-      "Object" in { an[MapEmptyException] should be thrownBy $.remove(MapPoint(1, 1)) }
-      "Between" in { an[MapEmptyException] should be thrownBy $.remove(DirectionalMapPoint(0, 0, Direction.Down)) }
+      "Object" in {an[MapEmptyException] should be thrownBy $.remove(MapPoint(1, 1))}
+      "Between" in {an[MapEmptyException] should be thrownBy $.remove(DirectionalMapPoint(0, 0, Direction.Down))}
     }
   }
   "Foreach" - {
@@ -60,9 +59,9 @@ abstract class BattleMapTest extends FreeSpec with AuxSpecs {
           val expectedBetweenSize = w * 2 + h * 2 + (w - 1) * h + (h - 1) * w
           val actualSize = map.betweens.map(_._1).toSet.size
           if (actualSize != expectedBetweenSize)
-             fail(s"For map of dimensions <$w, $h>, " +
-                 s"expected between size to be <$expectedBetweenSize> but was <$actualSize>. " +
-                 s"\nBetweens was equal to <${map.betweens.mkString("\n")}>")
+            fail(s"For map of dimensions <$w, $h>, " +
+                s"expected between size to be <$expectedBetweenSize> but was <$actualSize>. " +
+                s"\nBetweens was equal to <${map.betweens.mkString("\n")}>")
         }
       }
       "Set should be correct" in {
@@ -87,26 +86,24 @@ abstract class BattleMapTest extends FreeSpec with AuxSpecs {
     }
   }
   "Place" - {
-//    "Object" in { ??? }
     "Between" in {
       val pd = DirectionalMapPoint(0, 0, Direction.Down)
       createBattleMap(1, 2).place(pd, Wall)(pd) shouldReturn Wall
     }
   }
-  "toGraph" - {
-    "With Walls" in {
-      val place = createBattleMap(1, 2).place(DirectionalMapPoint(0, 0, Direction.Down), Wall)
-      place.toGraph.find(UnDiEdge(MapPoint(0, 0), MapPoint(0, 1))) shouldReturn None
-    }
-//    "Foo" in {
-//      val g = createBattleMap(2, 2).place(DirectionalMapPoint(0, 0, Direction.RIGHT), Wall).toGraph
-//      g.nodes.map(_.value) shouldSetEqual List(MapPoint(0, 0), MapPoint(1, 0), MapPoint(0, 1), MapPoint(1, 1))
-//      g.edges.map(e => UnDiEdge(e._1.value, e._2.value)) shouldSetEqual List(
-//        UnDiEdge(MapPoint(0, 0), MapPoint(0, 1)),
-//        UnDiEdge(MapPoint(0, 1), MapPoint(1, 1)),
-//        UnDiEdge(MapPoint(1, 1), MapPoint(1, 0))
-//      )
-//    }
+  "toGraph" in {
+    val g = createBattleMap(2, 2).place(DirectionalMapPoint(0, 0, Direction.Right), Wall).toGraph
+    g.nodes.map(_.value) shouldSetEqual Seq(
+      MapPoint(0, 0),
+      MapPoint(1, 0),
+      MapPoint(0, 1),
+      MapPoint(1, 1),
+    )
+    g.edges.map(e => UnDiEdge(e._1.value, e._2.value)) shouldSetEqual Seq(
+      UnDiEdge(MapPoint(0, 0), MapPoint(0, 1)),
+      UnDiEdge(MapPoint(0, 1), MapPoint(1, 1)),
+      UnDiEdge(MapPoint(1, 1), MapPoint(1, 0)),
+    )
   }
 }
 
