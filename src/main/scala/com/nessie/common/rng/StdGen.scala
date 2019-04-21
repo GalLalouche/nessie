5c55ec49f05e26ddd45458dc99e1c0eb6ea2f380
Gal PC <glpkmtg@gmmail.com>
2019-04-21 23:41:57 +0300

    StdGen

---
+++ src/main/scala/com/nessie/common/rng/StdGen.scala
@@ -0,0 +1,21 @@
+package com.nessie.common.rng
+
+import scala.util.Random
+
+class StdGen(val seed: Long ) {
+  def random: Random = new Random(seed)
+  def nextSeed: Long = next._1
+  def next: (Long, StdGen) = {
+    val nextSeed = random.nextLong()
+    nextSeed -> new StdGen(nextSeed)
+  }
+  def split: (StdGen, StdGen) = {
+    val nextSeed1 = random.nextLong()
+    val nextSeed2 = new Random(nextSeed1 + 1).nextLong()
+    new StdGen(nextSeed1) -> new StdGen(nextSeed2)
+  }
+}
+
+object StdGen {
+  def fromSeed(seed: Long) = new StdGen(seed)
+}
---
+++ src/test/scala/com/nessie/common/rng/StdGenTest.scala
@@ -0,0 +1,37 @@
+package com.nessie.common.rng
+
+import common.AuxSpecs
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalatest.PropSpec
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+class StdGenTest extends PropSpec with AuxSpecs with GeneratorDrivenPropertyChecks {
+  implicit lazy val arbSong: Gen[StdGen] = arbitrary[Long].map(StdGen.fromSeed)
+  property("Same next every time") {
+    forAll {rng: StdGen =>
+      rng.next._1 shouldReturn rng.next._1
+    }
+  }
+  property("Different items in next") {
+    forAll {rng: StdGen =>
+      rng.next._2.next._1 should not equal rng.next._1
+    }
+  }
+  property("Same next-next every time") {
+    forAll {rng: StdGen =>
+      rng.next._2.next._1 shouldReturn rng.next._2.next._1
+    }
+  }
+  property("Same split every time") {
+    forAll {rng: StdGen =>
+      rng.split._1.next._1 shouldReturn rng.split._1.next._1
+      rng.split._2.next._1 shouldReturn rng.split._2.next._1
+    }
+  }
+  property("Different items in split") {
+    forAll {rng: StdGen =>
+      rng.split._1.next._1 should not equal rng.split._2.next._1
+    }
+  }
+}
