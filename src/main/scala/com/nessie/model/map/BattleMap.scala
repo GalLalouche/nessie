c9f034c0eafae9c5feaa5035f0654fed6e711d43
Gal PC <glpkmtg@gmmail.com>
2019-04-24 11:59:15 +0300

    BattleMap.replaceSafely
    Also some other additional small refactorings

--- src/main/scala/com/nessie/model/map/BattleMap.scala
+++ src/main/scala/com/nessie/model/map/BattleMap.scala
@@ -1,8 +1,8 @@
 package com.nessie.model.map
 
-import com.nessie.common.rng.StdGen
 import common.rich.RichT._
 import common.rich.func.MoreSetInstances
+import common.rich.primitives.RichBoolean._
 import monocle.Lens
 import scalax.collection.GraphEdge.UnDiEdge
 import scalax.collection.immutable.Graph
@@ -11,57 +11,49 @@ import scala.util.Try
 
 import scalaz.syntax.ToFunctorOps
 
-/**
- * A map of a given level.
- * Is immutable.
- * @param width The map's width
- * @param height The map's height
- */
+/** An (immutable) map of a given level. */
 abstract class BattleMap(val width: Int, val height: Int)
     extends ToFunctorOps with MoreSetInstances {
   require(width > 0)
   require(height > 0)
 
-  def size = width * height
-  def replace(unitLocation: MapPoint, o: BattleMapObject): BattleMap =
-    remove(unitLocation).place(unitLocation, o)
+  protected def internalPlace(pd: DirectionalMapPoint, o: BetweenMapObject): BattleMap
+  protected def internalPlace(p: MapPoint, o: BattleMapObject): BattleMap
+
+  def size: Int = width * height
+  def replace(p: MapPoint, o: BattleMapObject): BattleMap = remove(p).place(p, o)
+  def replaceSafely(p: MapPoint, o: BattleMapObject): BattleMap = removeSafely(p).place(p, o)
+
+  def replace(dmp: DirectionalMapPoint, o: BetweenMapObject): BattleMap = remove(dmp).place(dmp, o)
+  def replaceSafely(dmp: DirectionalMapPoint, o: BetweenMapObject): BattleMap = removeSafely(dmp).place(dmp, o)
 
   def points: Iterable[(MapPoint, BattleMapObject)] =
-    for (y <- 0 until height; x <- 0 until width) yield
-      MapPoint(x, y).mapTo(p => p -> apply(p))
+    for (y <- 0 until height; x <- 0 until width) yield MapPoint(x, y) |-> apply
 
-  def isInBounds(p: MapPoint): Boolean =
-    p.x >= 0 && p.y >= 0 && p.x < width && p.y < height
+  def isInBounds(p: MapPoint): Boolean = p.x >= 0 && p.y >= 0 && p.x < width && p.y < height
 
   def apply(p: MapPoint): BattleMapObject
   def apply(pd: DirectionalMapPoint): BetweenMapObject
 
-  private def checkBounds(p: MapPoint) {
+  private def checkBounds(p: MapPoint): Unit =
     if (p.x >= width || p.y >= height)
       throw new IndexOutOfBoundsException(
         s"Point <$p> is out of bounds for map of dimensions <($width, $height)>")
-  }
   def place(p: MapPoint, o: BattleMapObject): BattleMap = {
     require(o != EmptyMapObject, "Don't place empty objects. Use remove instead.")
     checkBounds(p)
-    if (isOccupiedAt(p))
-      throw new MapOccupiedException(p)
-    else
-      internalPlace(p, o)
+    if (isOccupiedAt(p)) throw new MapOccupiedException(p) else internalPlace(p, o)
   }
   def place(pd: DirectionalMapPoint, o: BetweenMapObject): BattleMap = {
     checkBounds(pd.toPoint)
     require(o != EmptyBetweenMapObject, "Don't place empty objects. Use remove instead.")
-    if (isOccupiedAt(pd))
-      throw new MapOccupiedException(pd)
-    else
-      internalPlace(pd, o)
+    if (isOccupiedAt(pd)) throw new MapOccupiedException(pd) else internalPlace(pd, o)
   }
 
-  def isOccupiedAt(p: MapPoint): Boolean = !isEmptyAt(p)
-  def isOccupiedAt(pd: DirectionalMapPoint): Boolean = !isEmptyAt(pd)
   def isEmptyAt(p: MapPoint): Boolean = apply(p) == EmptyMapObject
   def isEmptyAt(pd: DirectionalMapPoint): Boolean = apply(pd) == EmptyBetweenMapObject
+  def isOccupiedAt(p: MapPoint): Boolean = isEmptyAt(p).isFalse
+  def isOccupiedAt(pd: DirectionalMapPoint): Boolean = isEmptyAt(pd).isFalse
 
   def remove(p: MapPoint): BattleMap =
     if (isOccupiedAt(p)) this.internalPlace(p, EmptyMapObject)
@@ -88,17 +80,14 @@ abstract class BattleMap(val width: Int, val height: Int)
        * @return The modified controller
        * @throws MapOccupiedException If there's already an object at dst
        */
-      def to(dst: MapPoint): BattleMap = {
-        remove(src).place(dst, o)
-      }
+      def to(dst: MapPoint): BattleMap = remove(src).place(dst, o)
     }
   }
 
   def betweens: Iterable[(DirectionalMapPoint, BetweenMapObject)] = points.map(_._1)
-      .flatMap(p => Direction.values.map(DirectionalMapPoint(p, _))).toSet.fproduct(apply)
-
-  protected def internalPlace(pd: DirectionalMapPoint, o: BetweenMapObject): BattleMap
-  protected def internalPlace(p: MapPoint, o: BattleMapObject): BattleMap
+      .flatMap(p => Direction.values.map(DirectionalMapPoint(p, _)))
+      .toSet
+      .fproduct(apply)
 
   lazy val toGraph: Graph[MapPoint, UnDiEdge] = {
     val vertices = points.map(_._1).toSet
@@ -119,14 +108,8 @@ abstract class BattleMap(val width: Int, val height: Int)
   /** Marks all points as a FullWall. */
   def fillItAll: BattleMap = points.map(_._1).foldLeft(this)(_.place(_, FullWall))
   /** Marks the points as a FullWall and places walls around it. */
-  def fill(next: MapPoint): BattleMap = {
-    // TODO extract to method
-    Direction.values.map(DirectionalMapPoint(next, _)).foldLeft(place(next, FullWall))(
-      // TODO replace safely
-      (map, dmp) => map.mapIf(_.isEmptyAt(dmp)).to(map.place(dmp, Wall)))
-
-  }
-
+  def fill(next: MapPoint): BattleMap =
+    DirectionalMapPoint.around(next).foldLeft(place(next, FullWall))(_.replaceSafely(_, Wall))
 
   def isBorder(dmp: DirectionalMapPoint): Boolean =
     (dmp.x == 0 && dmp.direction == Direction.Left) ||
@@ -143,6 +126,6 @@ abstract class BattleMap(val width: Int, val height: Int)
 }
 
 object BattleMap {
-  def pointLens(p: MapPoint) =
+  def pointLens(p: MapPoint): Lens[BattleMap, BattleMapObject] =
     Lens[BattleMap, BattleMapObject](_.apply(p))(o => m => m.internalPlace(p, o))
 }
