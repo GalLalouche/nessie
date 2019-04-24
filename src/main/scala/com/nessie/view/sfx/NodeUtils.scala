28e27d815034826e3df53693f5f47deb75aab185
Gal PC <glpkmtg@gmmail.com>
2019-04-24 21:16:05 +0300

    Fix stackoverflow since merge is implemented without a trampoline

--- src/main/scala/com/nessie/view/sfx/NodeUtils.scala
+++ src/main/scala/com/nessie/view/sfx/NodeUtils.scala
@@ -4,14 +4,18 @@ import com.nessie.model.units.CombatUnit
 import com.nessie.view.sfx.RichNode._
 import common.rich.func.MoreObservableInstances
 import rx.lang.scala.Observable
+import rx.lang.scala.subjects.PublishSubject
 import scalafx.scene.input.MouseEvent
 
 import scalaz.syntax.ToFunctorOps
 
 private object NodeUtils
     extends ToFunctorOps with MoreObservableInstances {
-  def mouseEvents[Key, N: NodeLike](m: Traversable[(Key, N)]): Observable[(MouseEvent, Key)] = m.map {
-    case (key, node) => implicitly[NodeLike[N]].scalaNode(node).mouseEvents.strengthR(key)
-  }.foldLeft[Observable[(MouseEvent, Key)]](Observable.empty)(_ merge _)
+  def mouseEvents[Key, N: NodeLike](ns: Traversable[(Key, N)]): Observable[(MouseEvent, Key)] = {
+    val $ = PublishSubject[(MouseEvent, Key)]()
+    // TODO move to RichObservable
+    for ((key, node) <- ns) implicitly[NodeLike[N]].scalaNode(node).mouseEvents.strengthR(key).subscribe($)
+    $
+  }
   def shortName(u: CombatUnit): String = u.metadata.name.take(3)
 }
