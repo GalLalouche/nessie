4855026f73f9e39b7c6a465956cd95f92541d472
Gal PC <glpkmtg@gmmail.com>
2019-04-27 02:05:07 +0300

    Add CreateRooms again for the new simple map

--- src/main/scala/com/nessie/model/map/gen/Room.scala
+++ src/main/scala/com/nessie/model/map/gen/Room.scala
@@ -1,15 +1,14 @@
 package com.nessie.model.map.gen
 
 import com.nessie.model.map.MapPoint
-import common.rich.primitives.RichBoolean._
 
 private case class Room(x: Int, y: Int, w: Int, h: Int) {
-  def pointNotInRectangle(p: MapPoint): Boolean = pointInRectangle(p).isFalse
   def pointInRectangle(p: MapPoint): Boolean = p.x >= x && p.x < x + w && p.y >= y && p.y < y + h
-  def noOverlap(room: Room): Boolean = room.mapPoints.forall(pointNotInRectangle)
+  // TODO implement more efficiently?
+  def isOverlapping(room: Room): Boolean = room.mapPoints.exists(pointInRectangle)
+  private def perimeterRoom = Room(x - 1, y - 1, w + 2, h + 2)
+  def isAdjacent(room: Room): Boolean = perimeterRoom.isOverlapping(room)
 
-  def mapPoints: Iterable[MapPoint] = for {
-    x <- this.x until this.x + w
-    y <- this.y until this.y + h
-  } yield MapPoint(x, y)
+  def mapPoints: Iterable[MapPoint] =
+    for (x <- this.x until this.x + w; y <- this.y until this.y + h) yield MapPoint(x, y)
 }
--- src/test/scala/com/nessie/model/map/gen/RoomTest.scala
+++ src/test/scala/com/nessie/model/map/gen/RoomTest.scala
@@ -11,34 +11,54 @@ class RoomTest extends FreeSpec with AuxSpecs {
   // _____
   private val $ = Room(x = 1, y = 1, w = 3, h = 2)
   "pointNotInRectangle" in {
-    $.pointNotInRectangle(MapPoint(1, 1)) shouldReturn false
-    $.pointNotInRectangle(MapPoint(1, 2)) shouldReturn false
+    $.pointInRectangle(MapPoint(1, 1)) shouldReturn true
+    $.pointInRectangle(MapPoint(1, 2)) shouldReturn true
 
-    $.pointNotInRectangle(MapPoint(2, 1)) shouldReturn false
-    $.pointNotInRectangle(MapPoint(2, 2)) shouldReturn false
+    $.pointInRectangle(MapPoint(2, 1)) shouldReturn true
+    $.pointInRectangle(MapPoint(2, 2)) shouldReturn true
 
-    $.pointNotInRectangle(MapPoint(3, 1)) shouldReturn false
-    $.pointNotInRectangle(MapPoint(3, 2)) shouldReturn false
+    $.pointInRectangle(MapPoint(3, 1)) shouldReturn true
+    $.pointInRectangle(MapPoint(3, 2)) shouldReturn true
 
-    $.pointNotInRectangle(MapPoint(0, 0)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(1, 0)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(2, 0)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(3, 0)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(4, 0)) shouldReturn true
+    $.pointInRectangle(MapPoint(0, 0)) shouldReturn false
+    $.pointInRectangle(MapPoint(1, 0)) shouldReturn false
+    $.pointInRectangle(MapPoint(2, 0)) shouldReturn false
+    $.pointInRectangle(MapPoint(3, 0)) shouldReturn false
+    $.pointInRectangle(MapPoint(4, 0)) shouldReturn false
 
-    $.pointNotInRectangle(MapPoint(0, 1)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(4, 1)) shouldReturn true
+    $.pointInRectangle(MapPoint(0, 1)) shouldReturn false
+    $.pointInRectangle(MapPoint(4, 1)) shouldReturn false
 
-    $.pointNotInRectangle(MapPoint(0, 2)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(4, 2)) shouldReturn true
+    $.pointInRectangle(MapPoint(0, 2)) shouldReturn false
+    $.pointInRectangle(MapPoint(4, 2)) shouldReturn false
 
-    $.pointNotInRectangle(MapPoint(0, 3)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(1, 3)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(2, 3)) shouldReturn true
-    $.pointNotInRectangle(MapPoint(3, 3)) shouldReturn true
+    $.pointInRectangle(MapPoint(0, 3)) shouldReturn false
+    $.pointInRectangle(MapPoint(1, 3)) shouldReturn false
+    $.pointInRectangle(MapPoint(2, 3)) shouldReturn false
+    $.pointInRectangle(MapPoint(3, 3)) shouldReturn false
   }
 
-  "noOverlap" in {
-    Room(0, 5, 10, 1).noOverlap(Room(5, 0, 1, 10)) shouldReturn false
+  "isOverlapping" in {
+    Room(0, 5, 10, 1).isOverlapping(Room(5, 0, 1, 10)) shouldReturn true
+  }
+  "isAdjacent" - {
+    "right" in {
+      $.isAdjacent(Room(x = 1, y = 3, w = 3, h = 2)) shouldReturn true
+    }
+    "left" in {
+      $.isAdjacent(Room(x = 0, y = 0, w = 1, h = 5)) shouldReturn true
+    }
+    "up" in {
+      $.isAdjacent(Room(x = 0, y = 0, w = 5, h = 1)) shouldReturn true
+    }
+    "down" in {
+      $.isAdjacent(Room(x = 3, y = 0, w = 5, h = 1)) shouldReturn true
+    }
+    "diagonal" in {
+      $.isAdjacent(Room(x = 4, y = 3, w = 1, h = 1)) shouldReturn true
+    }
+    "false" in {
+      $.isAdjacent(Room(x = 5, y = 5, w = 5, h = 5)) shouldReturn false
+    }
   }
 }
