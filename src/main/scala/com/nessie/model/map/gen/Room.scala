d5ea41a60e20487118e3fa29d6a35b5de02737a7
Gal Lalouche <gal.lalouche@oath.com>
2019-04-28 14:39:02 +0300

    More efficient implementation of isOverlapping

--- src/main/scala/com/nessie/model/map/gen/Room.scala
+++ src/main/scala/com/nessie/model/map/gen/Room.scala
@@ -4,11 +4,11 @@ import com.nessie.model.map.MapPoint
 
 private case class Room(x: Int, y: Int, w: Int, h: Int) {
   def pointInRectangle(p: MapPoint): Boolean = p.x >= x && p.x < x + w && p.y >= y && p.y < y + h
-  // TODO implement more efficiently?
-  def isOverlapping(room: Room): Boolean = room.mapPoints.exists(pointInRectangle)
+  def isOverlapping(other: Room): Boolean =
+    this.y <= other.y + other.h &&
+        this.y + this.h > other.y &&
+        this.x <= other.x + other.w &&
+        this.x + this.w > other.x
   private def perimeterRoom = Room(x - 1, y - 1, w + 2, h + 2)
   def isAdjacent(room: Room): Boolean = perimeterRoom.isOverlapping(room)
-
-  def mapPoints: Iterable[MapPoint] =
-    for (x <- this.x until this.x + w; y <- this.y until this.y + h) yield MapPoint(x, y)
 }
