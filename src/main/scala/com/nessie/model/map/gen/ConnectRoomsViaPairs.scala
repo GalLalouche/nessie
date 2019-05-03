3b53cc0a483aea6274f3d3da1e75f466b5f5dd32
Gal PC <glpkmtg@gmmail.com>
2019-05-03 14:37:33 +0300

    ConnectRoomsViaPairs implementation

--- src/main/scala/com/nessie/model/map/gen/ConnectRoomsViaPairs.scala
+++ src/main/scala/com/nessie/model/map/gen/ConnectRoomsViaPairs.scala
@@ -1,41 +1,87 @@
 package com.nessie.model.map.gen
 
+import com.nessie.common.graph.DfsTraversal
+import com.nessie.common.graph.RichUndirected._
 import com.nessie.common.rng.Rngable
 import com.nessie.common.rng.Rngable.ToRngableOps
-import com.nessie.model.map.{BattleMap, FullWall}
+import com.nessie.model.map.{BattleMap, FullWall, MapPoint}
+import common.rich.RichT._
 import common.rich.RichTuple._
+import common.rich.collections.LazyIterable
 import common.rich.collections.RichTraversableOnce._
-import common.rich.func.{MoreIterableInstances, ToMoreFoldableOps, ToMoreMonadPlusOps}
+import common.rich.func.{MoreIterableInstances, MoreSeqInstances, ToMoreFoldableOps, ToMoreMonadPlusOps}
 import common.uf.ImmutableUnionFind
 
+import scalaz.syntax.ToFunctorOps
+
 /**
- * Creates a BattleMap made up of just non-overlapping rooms. At the end of this procedure the
- * resulting [[BattleMap]] will be made up of [[FullWall]]s and [[RoomMapObject]]s.
+ * Connects rooms by iterating over pairs of unconnected rooms and digging a random path from one room to
+ * another. Completes when all rooms are connected together, at which point the resulting [[BattleMap]] will
+ * be made up of [[RoomMapObject]]s, [[TunnelMapObject]]s, and [[FullWall]]s.
  */
 private object ConnectRoomsViaPairs
-    extends ToMoreFoldableOps with ToMoreMonadPlusOps with MoreIterableInstances with ToRngableOps {
-  def go(map: BattleMap): Rngable[BattleMap] = {
+    extends ToMoreFoldableOps with ToMoreMonadPlusOps with MoreIterableInstances with ToRngableOps
+        with ToFunctorOps with MoreSeqInstances {
+  def go(map: BattleMap): Rngable[BattleMap] = iterate(map).map(_.last)
+
+  def iterate(map: BattleMap): Rngable[LazyIterable[BattleMap]] = Rngable.iterateOptionally(
     new Aux(
       map = map,
       rooms = ImmutableUnionFind(
         map.objects.view.map(_._2).select[RoomMapObject].map(_.index).toSet),
-    ).finish
-  }
+      index = 0,
+    )
+  )(_.next).map(_.map(_.map))
 
   private class Aux(
-      map: BattleMap,
+      val map: BattleMap,
       rooms: ImmutableUnionFind[Int],
+      index: Int,
   ) {
-    def finish: Rngable[BattleMap] = {
-      if (rooms.numberOfSets == 1) Rngable.pure(map) else for {
-        (r1, r2) <- rooms.values.unorderedPairs.toIterator.filterNot(_.reduce(rooms.sameSet)).sample
-        (map, rooms) <- Connector(map, rooms, r1, r2).go
-        result <- new Aux(map, rooms).finish
-      } yield result
-    }
+    def next: Rngable[Option[Aux]] = if (rooms.numberOfSets == 1) Rngable.pure(None) else for {
+      (r1, r2) <- rooms.values.unorderedPairs.toIterator.filterNot(_ reduce rooms.sameSet).sample
+      (map, rooms) <- Connector(map, rooms, r1, r2, RoomMapObject.getRooms(map), index).go
+    } yield Some(new Aux(map, rooms, index + 1))
   }
-  private case class Connector(map: BattleMap, rooms: ImmutableUnionFind[Int], r1: Int, r2: Int) {
+
+  private case class Connector(
+      map: BattleMap, roomConnections: ImmutableUnionFind[Int], r1: Int, r2: Int, rooms: Map[Int, Room],
+      index: Int) {
     def go: Rngable[(BattleMap, ImmutableUnionFind[Int])] =
-      if (rooms.sameSet(r1, r2)) Rngable.pure(map -> rooms) else ???
+      if (roomConnections.sameSet(r1, r2)) Rngable.pure(map -> roomConnections) else {
+        val startingPoints = {
+          val room2 = rooms(r2)
+          val sorted = getPerimeter(r1).fproduct(room2.distanceTo).sortBy(_.swap)
+          val min = sorted.head._2
+          sorted.takeWhile(_._2 == min).map(_._1)
+        }
+        for {
+          startingPoint <- startingPoints.sample
+          points = rooms(r1).mapPoints
+          path: LazyIterable[MapPoint] <- DfsTraversal(map.toFullGraph.removeNodes(points), startingPoint)
+        } yield {
+          val fullWalls = path.takeUntil(keepMoving).toVector
+          val connectedRooms = map.neighbors(fullWalls.last)
+              .map(map.apply)
+              .select[RoomMapObject]
+              .map(_.index)
+              .toVector
+          val nextUnionFind = connectedRooms.foldLeft(roomConnections)(_.union(r1, _))
+          val nextMap = fullWalls.foldLeft(map)(_.replaceSafely(_, TunnelMapObject(index)))
+          nextMap -> nextUnionFind
+        }
+      }
+
+    private def getPerimeter(i: Int): Seq[MapPoint] = {
+      val room = rooms(i)
+      (room.x.until(room.x + room.w).map(MapPoint(_, room.y - 1)) ++
+          room.x.until(room.x + room.w).map(MapPoint(_, room.y + room.h)) ++
+          room.y.until(room.y + room.h).map(MapPoint(room.x - 1, _)) ++
+          room.y.until(room.y + room.h).map(MapPoint(room.x + room.w, _))
+          ).filter(map.isInBounds)
+    }
+    private def keepMoving(mp: MapPoint): Boolean =
+      map(mp).eq(FullWall) &&
+          map.neighbors(mp).map(map.apply).flatMap(_.safeCast[RoomMapObject]).forall(_.index == r1)
   }
 }
