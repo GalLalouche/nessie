a958caa26f16a79ff3b2fc38f147fb450d378980
Gal PC <glpkmtg@gmmail.com>
2019-05-03 14:37:20 +0300

    Make DfsTraversal more deterministic by sorting neighbors

--- src/main/scala/com/nessie/common/graph/DfsTraversal.scala
+++ src/main/scala/com/nessie/common/graph/DfsTraversal.scala
@@ -8,20 +8,23 @@ import scalax.collection.Graph
 import scalax.collection.GraphEdge.UnDiEdge
 
 object DfsTraversal {
-  private case class Aux[A](graph: Graph[A, UnDiEdge], path: List[A], visited: Set[A]) extends ToRngableOps {
+  private case class Aux[A: Ordering](graph: Graph[A, UnDiEdge], path: List[A], visited: Set[A])
+      extends ToRngableOps {
     def head: A = path.head
     def next: Rngable[Option[Aux[A]]] = {
       val head :: tail = path
       if (visited(head)) copy(path = tail).next else {
-        val nextNeighbors = graph.get(head).~|.filterNot(visited)
+        // Sorting is required for reproducibility.
+        val nextNeighbors = graph.get(head).~|.filterNot(visited).view.map(_.toOuter).toVector.sorted
         if (nextNeighbors.isEmpty && tail.isEmpty) Rngable.pure(None) else nextNeighbors.shuffle
-            .map(_.map(_.toOuter).toList ++ tail)
+            .map(_.toList ++ tail)
             .map(Aux(graph, _, visited + head).opt)
       }
     }
   }
 
-  def apply[A](graph: Graph[A, UnDiEdge], startingPoint: A): Rngable[LazyIterable[A]] = Rngable
+  // A: Ordering is required for reproducibility.
+  def apply[A: Ordering](graph: Graph[A, UnDiEdge], startingPoint: A): Rngable[LazyIterable[A]] = Rngable
       .iterateOptionally(Aux[A](graph, List(startingPoint), Set.empty))(_.next)
       .map(_.map(_.head))
 }
