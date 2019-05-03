dd533cfdc894f36f572ce44aec36fe92a12d86b9
Gal PC <glpkmtg@gmmail.com>
2019-05-03 14:31:37 +0300

    Make StdGen a case class
    Nicer toString

--- src/main/scala/com/nessie/common/rng/StdGen.scala
+++ src/main/scala/com/nessie/common/rng/StdGen.scala
@@ -2,21 +2,21 @@ package com.nessie.common.rng
 
 import scala.util.Random
 
-class StdGen(val seed: Long ) {
+case class StdGen(seed: Long) {
   def random: Random = new Random(seed)
   def next: (Long, StdGen) = {
     val nextSeed = random.nextLong()
-    nextSeed -> new StdGen(nextSeed)
+    nextSeed -> StdGen(nextSeed)
   }
   def nextSeed: Long = next._1
   def nextGen: StdGen = next._2
   def split: (StdGen, StdGen) = {
     val nextSeed1 = random.nextLong()
     val nextSeed2 = new Random(nextSeed1 + 1).nextLong()
-    new StdGen(nextSeed1) -> new StdGen(nextSeed2)
+    StdGen(nextSeed1) -> StdGen(nextSeed2)
   }
 }
 
 object StdGen {
-  def fromSeed(seed: Long) = new StdGen(seed)
+  def fromSeed(seed: Long) = StdGen(seed)
 }
