5a36d8f2a51176307ff3988921575f53df9c4a35
Gal PC <glpkmtg@gmail.com>
2019-06-08 15:33:14 +0300

    Thicker Bresenham's lines

--- src/main/scala/com/nessie/model/map/fov/BresenhamsLine.scala
+++ src/main/scala/com/nessie/model/map/fov/BresenhamsLine.scala
@@ -1,13 +1,14 @@
 package com.nessie.model.map.fov
 
-import com.nessie.model.map.MapPoint
+import com.nessie.model.map.{Direction, MapPoint}
 
 import scala.collection.AbstractIterator
 
 /** See [[https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm]] */
-private object BresenhamsLine {
-  /** Returns all the points crossed by the straight line from p1 to p2. */
-  def apply(p0: MapPoint, p1: MapPoint): Iterable[MapPoint] = {
+object BresenhamsLine {
+  private def lineIterator(p0: MapPoint, p1: MapPoint): Iterator[MapPoint] = {
+    if (p0 == p1)
+      return Iterator(p0)
     // Modified implementation from https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#Scala
     val x0 = p0.x
     val y0 = p0.y
@@ -32,6 +33,39 @@ private object BresenhamsLine {
         $
       }
       def hasNext = sx * x <= sx * x1 && sy * y <= sy * y1
-    }.map(MapPoint.apply).toVector
+    }.map(MapPoint.apply)
+  }
+  /** Returns all the points crossed by the straight line from p1 to p2. */
+  def apply(p0: MapPoint, p1: MapPoint): Iterable[MapPoint] = lineIterator(p0, p1).toVector
+
+  /**
+   * Bresenham's algorithm is an OK approximation for line of sight, because photons are like, really small.
+   * But for movement, one cannot run diagonally from one cell to another if there isn't enough space.
+   * For example, suppose you have the following map, where you wish to get from S to T.
+   * *****--T
+   * S-----**
+   * It's not possible to run in a straight line from S to T due to the walls.
+   *
+   * This method therefore returns therefore returns all the cells that are necessary to be empty for such a
+   * movement, i.e., it tries to paint a "thick" line.
+   */
+  def thick(p0: MapPoint, p1: MapPoint): Iterable[MapPoint] = {
+    if (p0.x == p1.x || p0.y == p1.y)
+      return apply(p0, p1) // If there is a clear veritcal or horizontal line from p0 to p1, take it.
+
+    // As an approximation, we take the regular Bresenham's line, as well two additional lines, referred to in
+    // the code as "shadows". The y-shadow is start vertical to p0 and ends horizontal to p1. The x-shadow
+    // starts horizontal to p0 and ends vertical to p0. Both shadows are between p0 and p1.
+    // For example, if we want to draw a line from (0, 3) to (6, 0), the y-shadow will be
+    // from (0, 2) to (5, 0), and the x-shadow will be from (1, 3) to (6, 1).
+    val yShadow = lineIterator(
+      p0.go(if (p0.y < p1.y) Direction.Down else Direction.Up),
+      p1.go(if (p0.x < p1.x) Direction.Left else Direction.Right),
+    )
+    val xShadow = lineIterator(
+      p0.go(if (p0.x < p1.x) Direction.Right else Direction.Left),
+      p1.go(if (p0.y < p1.y) Direction.Up else Direction.Down),
+    )
+    (lineIterator(p0, p1) ++ xShadow ++ yShadow).toSet.toVector.sorted
   }
 }
---
+++ src/test/scala/com/nessie/model/map/fov/BresenhamsLineTest.scala
@@ -0,0 +1,19 @@
+package com.nessie.model.map.fov
+
+import com.nessie.model.map.MapPoint
+import common.AuxSpecs
+import org.scalatest.FreeSpec
+
+class BresenhamsLineTest extends FreeSpec with AuxSpecs {
+  // Shorter, for alignment
+  private def mp(x: Int, y: Int) = MapPoint(x = x, y = y)
+  "thick" - {
+    "Some test" in {
+      BresenhamsLine.thick(MapPoint(0, 2), MapPoint(4, 0)) shouldMultiSetEqual Vector(
+        /*               */ mp(2, 0), mp(3, 0), mp(4, 0),
+        mp(0, 1), mp(1, 1), mp(2, 1), mp(3, 1), mp(4, 1),
+        mp(0, 2), mp(1, 2), mp(2, 2), /*               */
+      )
+    }
+  }
+}
