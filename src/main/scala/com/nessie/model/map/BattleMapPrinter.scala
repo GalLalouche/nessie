3257297c15bee55784f2b8f0eb4ea43c9675233c
Gal PC <glpkmtg@gmail.com>
2020-04-03 11:44:50 +0300

    BattleMapPrint: support for extra tokens

--- src/main/scala/com/nessie/model/map/BattleMapPrinter.scala
+++ src/main/scala/com/nessie/model/map/BattleMapPrinter.scala
@@ -3,10 +3,20 @@ package com.nessie.model.map
 import com.nessie.model.map.GridPrinter.CharPrintable
 
 object BattleMapPrinter {
-  implicit val battleMapObjectEv: CharPrintable[BattleMapObject] = CharPrintable.apply {
-    case EmptyMapObject => '_'
-    case FullWall => '*'
-    case CombatUnitObject(u) => u.metadata.name.head
+  def apply(
+      m: BattleMap,
+      additionalCharacters: PartialFunction[BattleMapObject, Char] = PartialFunction.empty,
+  ): String = {
+    implicit val battleMapObjectEv: CharPrintable[BattleMapObject] = CharPrintable.apply {o =>
+      additionalCharacters.lift(o).getOrElse {
+        o match {
+          case EmptyMapObject => '_'
+          case FullWall => '*'
+          case CombatUnitObject(u)
+          => u.metadata.name.head
+        }
+      }
+    }
+    GridPrinter(m.grid)
   }
-  def apply(m: BattleMap): String = GridPrinter(m.grid)
 }
